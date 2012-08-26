2a9404c20d7c5f9bf19909fce28740bc1d8d119f
datapoltiical <datapolitical@gmail.com>
2012-08-26 17:14:59 +0200

    octopress test

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/octocfn/"
+http_images_path = "/octocfn/images"
+http_fonts_path = "/octocfn/fonts"
+css_dir = "public/octocfn/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
